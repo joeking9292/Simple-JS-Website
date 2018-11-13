b385036a640b29c63e066e7ecea3ccccac7a1a5c
Joseph Noonan <noonanj@flip2.engr.oregonstate.edu>
2018-11-12 19:07:07 -0800

    Pushing most of the code

--- server.js
+++ server.js
@@ -2,6 +2,48 @@
  * Write your server code in this file.  Don't forget to add your name and
  * @oregonstate.edu email address below.
  *
- * name:
- * email:
+ * name: Joseph Noonan
+ * email: noonanj@oregonstate.edu
  */
+
+ var http = require('http');
+ var fs = require('fs');
+ var PORT = process.env.PORT || 3000;
+
+ var server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    switch(req.url){
+        case "/style.css":
+            res.end(fs.readFileSync("public/style.css"));
+            console.log("== loaded style.css");
+            break;
+        case "/index.html":
+            res.end(fs.readFileSync("public/index.htlm"));
+            console.log("== loaded index.html");
+            break;
+        case "/":
+            res.end(fs.readFileSync("public/index.html"));
+            console.log("== loaded index.html");
+            break;
+        case "/index.js":
+            res.end(fs.readFileSync("public/index.js"));
+            console.log("== loaded index.js");
+            break;
+        case "/benny.jpg":
+            res.end(fs.readFileSync("public/benny.jpg"));
+            console.log("== loaded benny.jpg");
+            break;
+        default:
+            res.end(fs.readFileSync("public/404.html"));
+            res.statusCode = 404;
+            console.log("== loaded 404.html");
+            break;
+    }
+ });
+
+ server.listen(PORT, function (err) {
+     if (err) {
+         throw err;
+     }
+     console.log("== Server listening on port 3000");
+ });
